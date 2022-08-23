5bbc33b4553b6d09c44edd2603407021b7a05844
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-08-23 06:46:31 -0400

    Wrote contracts tests using Mocha

---
+++ .history/test/Token_20220823063827.js
@@ -0,0 +1,27 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('Token contract', function () {
+    async function deployTokenFixture() {
+        const Token = await ethers.getContractFactory('Token');
+        const [owner, addr1, addr2] = await ethers.getSigners();
+
+        const hardhatToken = await Token.deploy();
+
+        await hardhatToken.deployed();
+
+        return { Token, hardhatToken, owner, addr1, addr2 };
+    }
+    it("Should assign the total supply of tokens to the owner", async function () {
+        const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
+
+        const ownerBalance = await hardhatToken.balanceOf(owner);
+        expect(hardhatToken.totalSupply()).to.eq(ownerBalance);
+    });
+    it("Should transfer tokens from the owner to another address", async function () {
+        const { hardhatToken, owner, addr1, addr2 } = await loadFixture(deployTokenFixture);
+
+        const ownerBalance = await hardhatToken.balanceOf(owner);
+    });
+})
